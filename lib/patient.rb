2dbba25e305dec79a4e72eb411d5f68141b793f4
Joshua Palacios <palacios.joshua@gmail.com>
2017-11-08 04:51:30 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,7 +1,13 @@
 class Patient
-  attr_accessor :name, :doctor, :appointment
+  attr_accessor :name, :doctor, :appointments
 
   def initialize(name)
     @name = name
+    @appointments = []
+  end
+
+  def add_appointment(appointment)
+    @appointments << appointment
+    appointment.patient = self
   end
 end
